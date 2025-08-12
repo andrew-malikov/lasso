568ba9ab685290faa48381233adfa38b840fe0eb
Andrew Malikov <andrewmalikov@sent.com>
2025-08-12 12:35:05 +0300

    feat: update microservices ports in api gateway in compose

--- ApiGateway.WebApi/Program.cs
+++ ApiGateway.WebApi/Program.cs
@@ -1,9 +1,18 @@
 using ApiGateway.WebApi;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Microsoft.Extensions.Options;
 using Users.Grpc.User;
 using static Users.Grpc.User.UserService;
+using static Finance.Grpc.Currency.CurrencyService;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(provider =>
+{
+    var factory = provider.GetRequiredService<ILoggerFactory>();
+    return factory.CreateLogger("ApiGateway");
+});
 
 builder.Services.Configure<GrpcServiceOptions>("UserGrpc",
     builder.Configuration.GetSection("UserGrpc")
@@ -19,6 +28,10 @@ builder.Services.AddGrpcClient<UserServiceClient>((services, options) =>
 {
     options.Address = services.GetRequiredService<IOptionsSnapshot<GrpcServiceOptions>>().Value.Address;
 });
+builder.Services.AddGrpcClient<CurrencyServiceClient>((services, options) =>
+{
+    options.Address = services.GetRequiredService<IOptionsSnapshot<GrpcServiceOptions>>().Value.Address;
+});
 
 var app = builder.Build();
 
@@ -30,12 +43,99 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/users",
-        async (UserServiceClient usersBase) =>
+app.MapPost("/api/users/register", async (RegisterUserRequest request, UserServiceClient grpcClient) =>
+    {
+        var grpcResponse = await grpcClient.RegisterAsync(request);
+        return grpcResponse.ResultCase switch
         {
-            await usersBase.RegisterAsync(new RegisterUserRequest { Username = "wow", Password = "wow" });
-        })
+            RegisterUserResponse.ResultOneofCase.SuccessfullyRegistered => Results.Ok(),
+
+            RegisterUserResponse.ResultOneofCase.FailureMessage => Results.BadRequest(new
+                { error = grpcResponse.FailureMessage }),
+
+            _ => Results.StatusCode(500)
+        };
+    })
     .WithName("RegisterUser")
     .WithOpenApi();
 
+app.MapPost("/api/users/login", async (LoginRequest request, UserServiceClient grpcClient) =>
+    {
+        var grpcResponse = await grpcClient.LoginAsync(request);
+        return grpcResponse.ResultCase switch
+        {
+            LoginResponse.ResultOneofCase.Tokens
+                => Results.Ok(new
+                {
+                    access_token = grpcResponse.Tokens.AccessToken,
+                    refresh_token = grpcResponse.Tokens.RefreshToken
+                }),
+
+            LoginResponse.ResultOneofCase.Failure
+                => Results.BadRequest(new { error = grpcResponse.Failure.Message }),
+
+            _ => Results.StatusCode(500)
+        };
+    })
+    .WithName("UserLogin")
+    .WithOpenApi();
+
+app.MapPost("/api/users/logout", async (LogoutRequest request, UserServiceClient grpcClient) =>
+    {
+        await grpcClient.LogoutAsync(request);
+        return Results.Ok();
+    })
+    .WithName("UserLogout")
+    .WithOpenApi();
+
+
+app.MapGet("/api/currencies/favorites",
+        async (HttpContext httpContext, CurrencyServiceClient grpcClient, ILogger logger) =>
+        {
+            var authHeader = httpContext.Request.Headers["Authorization"].ToString();
+            if (string.IsNullOrWhiteSpace(authHeader) || !authHeader.StartsWith("Bearer "))
+            {
+                return Results.Unauthorized();
+            }
+
+            var metadata = new Metadata
+            {
+                { "Authorization", authHeader }
+            };
+
+            try
+            {
+                var grpcResponse = await grpcClient.GetFavoritesAsync(new Empty(), new CallOptions(headers: metadata));
+
+                var result = grpcResponse.Currencies.Select(c => new CurrencyResponse
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Rate = c.Rate
+                });
+
+                return Results.Ok(result);
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Unauthenticated)
+            {
+                return Results.Unauthorized();
+            }
+            catch (RpcException ex)
+            {
+                logger.LogError(ex, "An error occured during handling GRPC request/response.");
+                return Results.StatusCode(500);
+            }
+        })
+    .WithName("UserFavouriteCurrencies")
+    .WithOpenApi();
+
 app.Run();
+
+internal class CurrencyResponse
+{
+    public required string Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Rate { get; init; }
+}
